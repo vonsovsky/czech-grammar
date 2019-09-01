6fe814c6becd6ef2d0b9d1262d20d703585b45ac
Jakub Vonšovský <jakub.vonsovsky@neuronsw.com>
2019-09-01 20:26:13 +0200

    Reformat code in Visual Code

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import React from "react";
+import { Platform } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import HomeScreen from '../screens/HomeScreen';
-import GameScreen from '../screens/GameScreen';
-import ProfileScreen from '../screens/ProfileScreen';
+import HomeScreen from "../screens/HomeScreen";
+import GameScreen from "../screens/GameScreen";
+import ProfileScreen from "../screens/ProfileScreen";
 
 const MainNavigator = createStackNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: {
-      header: null,
-    },
+      header: null
+    }
   },
-  Game: {screen: GameScreen},
-  Profile: {screen: ProfileScreen},
+  Game: { screen: GameScreen },
+  Profile: { screen: ProfileScreen }
 });
 
 const App = createAppContainer(MainNavigator);
